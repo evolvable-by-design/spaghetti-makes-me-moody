00422bf16be9521e7daa4f3c7714a89672e1cc78
Cody Howarth <codyhowarth@gmail.com>
2018-02-25 13:53:22 -0500

    Remove - redundant anonymous function

--- src/components/JournalView.js
+++ src/components/JournalView.js
@@ -28,49 +28,47 @@ class JournalView extends React.Component {
     this.setState({ entryValue: event.target.value });
   }
 
-  handleSubmit(props) {
+  handleSubmit(event) {
     var self = this;
-    return event => {
-      event.preventDefault();
-      let postData = {
-        document: {
-          type: 'PLAIN_TEXT',
-          content: self.state.entryValue
-        }
-      };
-
-      axios
-        .post(
-          'https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBAGVnmL3X96nCv0GLAxuFw4-czBVTrfyo',
-          postData
-        )
-        .then(function(response) {
-          let historyData = {
-            date: new Date(),
-            entry: self.state.entryValue,
-            responseData: response.data
-          };
-
-          if (
-            window.confirm(
-              'Would you like to view the results of your entry analysis now?'
-            )
-          ) {
-            // Set data and move to history view - currently just sending the
-            // entire response until we keep track of entries
-            props.setHistoryData(historyData);
-            props.changeView('History');
-          } else {
-            // Add data but don't change view
-            props.setHistoryData(historyData);
-            // Reset entry box
-            self.resetEntryBox();
-          }
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+    event.preventDefault();
+    let postData = {
+      document: {
+        type: 'PLAIN_TEXT',
+        content: self.state.entryValue
+      }
     };
+
+    axios
+      .post(
+        'https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBAGVnmL3X96nCv0GLAxuFw4-czBVTrfyo',
+        postData
+      )
+      .then(function(response) {
+        let historyData = {
+          date: new Date(),
+          entry: self.state.entryValue,
+          responseData: response.data
+        };
+
+        if (
+          window.confirm(
+            'Would you like to view the results of your entry analysis now?'
+          )
+        ) {
+          // Set data and move to history view - currently just sending the
+          // entire response until we keep track of entries
+          self.props.setHistoryData(historyData);
+          self.props.changeView('History');
+        } else {
+          // Add data but don't change view
+          self.props.setHistoryData(historyData);
+          // Reset entry box
+          self.resetEntryBox();
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   render() {
@@ -78,7 +76,7 @@ class JournalView extends React.Component {
       <div>
         <div style={textBoxLayoutStyle}>
           <b class="MainBodyText">how was your day? :)</b>
-          <form onSubmit={this.handleSubmit(this.props)}>
+          <form onSubmit={this.handleSubmit}>
             <label>
               <textarea
                 class="JournalBox"
