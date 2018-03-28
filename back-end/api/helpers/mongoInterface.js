97ca1944ce4ee8cb2613d8a654d4cf04caf6d5f8
Zachary Medeiros <zachary.medeiros@mail.utoronto.ca>
2018-03-28 14:11:43 -0400

    Started working on Update

--- back-end/api/helpers/mongoInterface.js
+++ back-end/api/helpers/mongoInterface.js
@@ -85,7 +85,52 @@ const retrieveUser = async function(username, password) {
   client.close();
 };
 
+const updateUser = async function(username, password, entry){
+  let foundUser = false;
+  let client;
+
+  try {
+    client = await MongoClient.connect(url);
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbName);
+
+    // Get the collection
+    const col = db.collection(USER_COLLECTION);
+
+    // Get first two documents that match the query
+    const docs = await col.find({ userName: username }).toArray();
+    //assert.equal(2, docs.length);
+    if (docs.length < 1) {
+      return 1;
+    }
+
+    // Name doesn't exist so insert the user into the db
+    // let r = await col.insertOne({
+    //   userName: username,
+    //   password: password,
+    //   entryList: []
+    // });
+    // assert.equal(1, r.insertedCount);
+    // return 0;
+
+    //Update the entrylist of the User
+    let r = await col.updateOne({
+      entrylist: dos[0].entryList.append(entry)
+    });
+    
+    assert.equal(1, r.updateCount);
+    return 0;
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  // Close connection
+  client.close();
+};
+
 module.exports = {
   createUser: createNewUser,
-  retrieveUser: retrieveUser
+  retrieveUser: retrieveUser,
+  updateUser: updateUser
 };
