a1eeaec813a147ee3c3468aff261b3e8328b5f11
Calvin Ng <calvin.ng96@gmail.com>
2018-04-01 11:17:06 -0400

    Remove cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,15 +1,12 @@
 'use strict';
 
 const SwaggerExpress = require('swagger-express-mw');
-const cors = require('cors');
 const app = require('express')();
 module.exports = app; // for testing
 
 const config = {
   appRoot: __dirname // required config
 };
-
-app.use(cors());
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) {
